8992f59dcece5d481732d1659d3916c1226f597e
Greg <greghatzis@gmail.com>
2019-07-13 00:53:47 +0300

    Remove the Task object and moved the task execution to a top level method because they have self wich is not picklable and Processes do not accept it.

--- playground.py
+++ playground.py
@@ -13,6 +13,11 @@ def add():
     return some_data
 
 
+def simple_task():
+    print('==> Simple task running.')
+    return True
+
+
 def a_callback(future):
     time.sleep(0.5)
     print('==> Callback called.')
@@ -41,7 +46,7 @@ def task_time():
 
 def task_args(msg, **kwargs):
     print('==>', msg, 'Running')
-    time.sleep('1')
+    time.sleep(1)
     print('==>', msg, 'Finished')
     return "task_args return msg: {}".format(msg)
 
@@ -51,20 +56,25 @@ def t_exep():
 
 
 if __name__ == "__main__":
-    rn = Runium()
-    # r1 = rn.run_thread()
-    # r1 = rn.run(task, mode='thread')
+    rnp = Runium(concurrency_mode="multiprocessing")
+    rnt = Runium()
+
+    rp1 = rnp.run(
+        simple_task, callback=b_callback
+    ).result()
+    rt1 = rnt.run(task_args, kwargs={'msg': 'RT1'}).result()
+
     # r1 = rn.run(task_args, kwargs={'msg': 'R1'}, callback=a_callback)
     # r2 = rn.run(task, kwargs={'msg': 'R2'})
     # r1 = rn.run(task_time, every='1 seconds', times=5).then(a_callback)
 
-    r1 = rn.run(
-        task_args, kwargs={'msg': 'R1'}, times=3, exit_on_exception=False
-    )
+    # r1 = rn.run(
+    #     task_args, kwargs={'msg': 'R1'}, times=3, exit_on_exception=False
+    # )
     # r1.when_finished(b_callback)
 
-    print(r1.result())
-    print(r1.runs_count)
+    # print(r1.result())
+    # print(r1.runs_count)
     # print(r1.thread)
     # print('==> Is running:', r1.running())
     # print(r1.result())
--- runium/core.py
+++ runium/core.py
@@ -4,9 +4,8 @@ This is the main module.
 
 import time
 import traceback
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 import threading
-import uuid
 from runium.util import get_seconds
 
 
@@ -15,56 +14,37 @@ class Runium(object):
     This is the main class that gets instantiated by the user.
     """
 
-    def __init__(self, concurrency_mode='threads', workers=None):
+    def __init__(self, concurrency_mode='multithreading', workers=None):
         self.tasks = {}
-        self._executor = ThreadPoolExecutor(workers)
+        self.workers = workers
+        self.concurrency_mode = concurrency_mode
+        if self.concurrency_mode == 'multiprocessing':
+            self._executor = ProcessPoolExecutor(self.workers)
+        else:
+            self._executor = ThreadPoolExecutor(self.workers)
 
     def run(
         self, task, every=None, times=None, start_in=0,
-        kwargs={}, exit_on_exception=True
+        kwargs={}, callback=None, exit_on_exception=True
     ):
         """
         Creates a new Task, adds it to the tasks list and submits it to the
         PoolExecutor.
         Returns the Task object.
         """
+        every = get_seconds(every)
+        start_in = get_seconds(start_in)
         every, times = self.__set_every_times_defaults(every, times)
-        current_task = Task(
-            task, kwargs, every, times, start_in, exit_on_exception
+
+        future = self._executor.submit(
+            _run_task,
+            task, every, times, start_in, kwargs, exit_on_exception
         )
 
-        future = self._executor.submit(self.__loop, current_task)
-        current_task.future = future
+        if callback is not None:
+            future.add_done_callback(callback)
 
-        self.tasks[current_task.id] = current_task
-        return current_task
-
-    def __loop(self, task):
-        """
-        Runs a task in :start_in seconds, every :interval seconds for :times
-        times.
-        If times is set to 0 it loops indefinitely.
-        """
-        task_result = None
-        interval = task.interval
-
-        if task.start_in > 0:
-            time.sleep(task.start_in)
-
-        next_time = time.time() + interval
-        while True:
-            task.runs_count += 1
-            task_result = task.run()
-            if task.times > 0 and task.runs_count >= task.times:
-                break
-            # Skip tasks if we are behind schedule:
-            if interval > 0:
-                next_time +=\
-                    (time.time() - next_time) // interval * interval + interval
-                time.sleep(max(0, next_time - time.time()))
-
-        self.__remove_task_from_list(task.id)
-        return task_result
+        return future
 
     def __set_every_times_defaults(self, every, times):
         """
@@ -84,135 +64,45 @@ class Runium(object):
             every = 0
         return every, times
 
-    def __remove_task_from_list(self, task_id):
-        try:
-            del self.tasks[task_id]
-        except KeyError:
-            pass
 
-
-class Task(object):
+def _run_task(
+    fn, interval, times, start_in, kwargs, exit_on_exception=True
+):
     """
-    This object represents the method that is beeing run by Runium.
-    Every time we tell Runnium to run a method, it creates and returns a Task
-    object.
-    It contains usefull data and methods for Runium and the users and exposes
-    the future and the current Thread objects.
+    Runs the task, optionally for :times every :every seconds in :start_in
+    seconds.
+    This is blocking and is meant to be called by Runium in a new Thread or
+    Process.
     """
+    task_result = None
+    runs_count = 0
 
-    def __init__(
-        self, fn, arguments, interval, times, start_in, exit_on_exception
-    ):
-        self.id = uuid.uuid1().int
-        self.fn = fn
-        self.arguments = arguments,
-        self.interval = get_seconds(interval)
-        self.times = times
-        self.start_in = get_seconds(start_in)
-        self.exit_on_exception = exit_on_exception
-        self.callback = None
-        self.future = None
-        self.thread = None
-        self.runs_count = 0
+    if start_in > 0:
+        time.sleep(start_in)
 
-    def run(self):
-        """
-        Runs the task. This is a blocking operation. Do not call this method.
-        It is called by Runium internally.
-        """
-        kwargs = self.arguments[0]
-        self.thread = threading.current_thread()
+    next_time = time.time() + interval
+    while True:
+        runs_count += 1
+        # TODO methods with no **kwargs will raise TypeError.
+        kwargs['runs_count'] = runs_count
 
-        if self.exit_on_exception is False:
-            result = None
+        # The actual execution of the task takes place here.
+        if exit_on_exception is False:
             try:
-                result = self.fn(**kwargs)
+                task_result = fn(**kwargs)
             except Exception as err:
                 traceback.print_exc()
-                # Will try to add this later since it gives wrong results to 
-                # the callback.
-                # self.future.set_exception(err)
-                result = err
-            finally:
-                return result
+                task_result = err
         else:
-            return self.fn(**kwargs)
+            task_result = fn(**kwargs)
 
-    # The following methods are wrappers for the future object.
-    def cancel(self):
-        """
-        Attempt to cancel the call. If the call is currently being executed or
-        finished running and cannot be cancelled then the method will return
-        False, otherwise the call will be cancelled and the method will return
-        True.
-        """
-        return self.future.cancel()
+        if times > 0 and runs_count >= times:
+            break
 
-    def cancelled(self):
-        """
-        Return True if the call was successfully cancelled.
-        """
-        return self.future.cancelled()
+        # Skip tasks if we are behind schedule:
+        if interval > 0:
+            next_time +=\
+                (time.time() - next_time) // interval * interval + interval
+            time.sleep(max(0, next_time - time.time()))
 
-    def running(self):
-        """
-        Return True if the call is currently being executed and cannot be
-        cancelled.
-        """
-        return self.future.running()
-
-    def done(self):
-        """
-        Return True if the call was successfully cancelled or finished running.
-        """
-        return self.future.done()
-
-    def result(self, timeout=None):
-        """
-        Return the value returned by the call. If the call hasn’t yet completed
-        then this method will wait up to timeout seconds. If the call hasn’t
-        completed in timeout seconds, then a concurrent.futures.TimeoutError
-        will be raised. timeout can be an int or float. If timeout is not
-        specified or None, there is no limit to the wait time.
-
-        If the future is cancelled before completing then CancelledError will
-        be raised.
-
-        If the call raised, this method will raise the same exception.
-        """
-        return self.future.result(timeout)
-
-    def exception(self, timeout=None):
-        """
-        Return the exception raised by the call. If the call hasn’t yet
-        completed then this method will wait up to timeout seconds. If the call
-        hasn’t completed in timeout seconds, then a
-        concurrent.futures.TimeoutError will be raised. timeout can be an int
-        or float. If timeout is not specified or None, there is no limit to the
-        wait time.
-
-        If the future is cancelled before completing then CancelledError will
-        be raised.
-
-        If the call completed without raising, None is returned.
-        """
-        return self.future.exception(timeout)
-
-    def when_finished(self, callback):
-        """
-        Attaches the callback onto the future. callback will be called, with
-        the future as its only argument, every time the future is cancelled or
-        finishes running.
-
-        Added callables are called in the order that they were added and are
-        always called in a thread belonging to the process that added them. If
-        the callable raises an Exception subclass, it will be logged and
-        ignored. If the callable raises a BaseException subclass, the behavior
-        is undefined.
-
-        If the future has already completed or been cancelled, callback will be
-        called immediately.
-        """
-        self.callback = callback
-        self.future.add_done_callback(callback)
-        return self
+    return task_result
