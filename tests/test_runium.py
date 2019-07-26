ba89015859976d3426d25a53af5fa4d8827c7483
Grigoris Chatzinikolaou <greghatzis@gmail.com>
2019-07-26 11:31:05 +0300

    Remove playground. Better tests coverage.

--- tests/test_runium.py
+++ tests/test_runium.py
@@ -13,128 +13,178 @@ def rnp():
     return Runium(mode='multiprocessing')
 
 
-def test_run_3_times(rnt, rnp):
-    rt = rnt.new_task(runnium_param).run(times=3).result()
-    rp = rnp.new_task(runnium_param).run(times=3).result()
-    assert rt['iterations'] == 3
-    assert rp['iterations'] == 3
+class TestTimes():
+    def test_threading(self, rnt):
+        rt = rnt.new_task(runnium_param).run(times=3).result()
+        assert rt['iterations'] == 3
+
+    def test_processing(self, rnp):
+        rp = rnp.new_task(runnium_param).run(times=3).result()
+        assert rp['iterations'] == 3
 
 
-def test_loop_drift(rnt, rnp):
-    prev_time = time.time()
-    rnt.new_task(simple_task).run(every=0.1, times=10).result()
-    time_elapsed = time.time() - prev_time
-    assert time_elapsed < 1
-    assert time_elapsed > 0.88
-    prev_time = time.time()
-    rnp.new_task(simple_task).run(every=0.1, times=10).result()
-    time_elapsed = time.time() - prev_time
-    assert time_elapsed < 1
-    assert time_elapsed > 0.88
+class TestLoopDrift():
+    def test_threading(selft, rnt):
+        prev_time = time.time()
+        rnt.new_task(simple_task).run(every=0.1, times=10).result()
+        time_elapsed = time.time() - prev_time
+        is_ok = (time_elapsed < 1) and (time_elapsed > 0.88)
+        assert is_ok is True
+
+    def test_processing(selft, rnp):
+        prev_time = time.time()
+        rnp.new_task(simple_task).run(every=0.1, times=10).result()
+        time_elapsed = time.time() - prev_time
+        is_ok = (time_elapsed < 1) and (time_elapsed > 0.88)
+        assert is_ok is True
 
 
-def test_start_in(rnt, rnp):
-    prev_time = time.time()
-    rnt.new_task(simple_task).run(start_in=0.1).result()
-    time_elapsed = time.time() - prev_time
-    assert time_elapsed < 0.11
-    assert time_elapsed > 0.09
-    prev_time = time.time()
-    rnp.new_task(simple_task).run(start_in=0.1).result()
-    time_elapsed = time.time() - prev_time
-    assert time_elapsed < 0.11
-    assert time_elapsed > 0.09
+class TestStartIn():
+    def test_threading(self, rnt):
+        prev_time = time.time()
+        rnt.new_task(simple_task).run(start_in=0.1).result()
+        time_elapsed = time.time() - prev_time
+        is_ok = (time_elapsed < 0.11) and (time_elapsed > 0.09)
+        assert is_ok is True
+
+    def test_processing(self, rnp):
+        prev_time = time.time()
+        rnp.new_task(simple_task).run(start_in=0.1).result()
+        time_elapsed = time.time() - prev_time
+        is_ok = (time_elapsed < 0.11) and (time_elapsed > 0.09)
+        assert is_ok is True
 
 
-def test_skipping_tasks(rnt, rnp):
-    prev_time = time.time()
-    rnt.new_task(sleepy_task).run(every=0.1, times=2).result()
-    time_elapsed = time.time() - prev_time
-    assert time_elapsed < 0.51
-    assert time_elapsed > 0.2
-    prev_time = time.time()
-    rnp.new_task(sleepy_task).run(every=0.1, times=2).result()
-    time_elapsed = time.time() - prev_time
-    assert time_elapsed < 0.51
-    assert time_elapsed > 0.2
+class TestTaskSkipping():
+    def test_threading(self, rnt):
+        prev_time = time.time()
+        rnt.new_task(sleepy_task).run(every=0.1, times=2).result()
+        time_elapsed = time.time() - prev_time
+        is_ok = (time_elapsed < 0.51) and (time_elapsed > 0.2)
+        assert is_ok is True
+
+    def test_processing(self, rnp):
+        prev_time = time.time()
+        rnp.new_task(sleepy_task).run(every=0.1, times=2).result()
+        time_elapsed = time.time() - prev_time
+        is_ok = (time_elapsed < 0.51) and (time_elapsed > 0.2)
+        assert is_ok is True
 
 
-def test_kwargs(rnt, rnp):
-    rt = rnt.new_task(
-        task_with_kwargs, kwargs={'msg': 'Spam, Spam, Spam, egg and Spam'}
-    ).run().result()
-    rp = rnp.new_task(
-        task_with_kwargs, kwargs={'msg': 'Spam, Spam, Spam, egg and Spam'}
+class TestKwargs():
+    def test_threading(self, rnt):
+        r = rnt.new_task(
+            task_with_kwargs, kwargs={'msg': 'Spam, Spam, Spam, egg and Spam'}
+        ).run().result()
+        assert r == 'Spam, Spam, Spam, egg and Spam'
 
-    ).run().result()
-    assert rt == 'Spam, Spam, Spam, egg and Spam'
-    assert rp == 'Spam, Spam, Spam, egg and Spam'
-
-
-def test_return_exception(rnt, rnp):
-    rt = rnt.new_task(task_with_exception).run().result()
-    rp = rnp.new_task(task_with_exception).run().result()
-    assert type(rt).__name__ == 'ValueError'
-    assert type(rp).__name__ == 'ValueError'
+    def test_processing(self, rnp):
+        r = rnp.new_task(
+            task_with_kwargs, kwargs={'msg': 'Spam, Spam, Spam, egg and Spam'}
+        ).run().result()
+        assert r == 'Spam, Spam, Spam, egg and Spam'
 
 
-def test_run_no_kwargs_bleeding(rnt, rnp):
-    assert rnt.new_task(simple_task).run().result() is True
-    assert rnp.new_task(simple_task).run().result() is True
+class TestReturnException():
+    def test_threading(self, rnt):
+        r = rnt.new_task(task_with_exception).run().result()
+        assert type(r).__name__ == 'ValueError'
+
+    def test_processing(self, rnp):
+        r = rnp.new_task(task_with_exception).run().result()
+        assert type(r).__name__ == 'ValueError'
 
 
-def test_on_finished_success_callback(rnt, rnp):
-    assert rnt.new_task(task_callback_success).on_finished(
-        se_callback, updates_result=True
-    ).run().result() == 'Success'
-    assert rnp.new_task(task_callback_success).on_finished(
-        se_callback, updates_result=True
-    ).run().result() == 'Success'
+class TestKwargsBleeding():
+    def test_threading(self, rnt):
+        assert rnt.new_task(simple_task).run().result() is True
+
+    def test_processing(self, rnp):
+        assert rnp.new_task(simple_task).run().result() is True
 
 
-def test_on_finished_error_callback(rnt, rnp):
-    assert rnt.new_task(task_callback_error).on_finished(
-        se_callback, updates_result=True
-    ).run().result() == 'Error'
-    assert rnp.new_task(task_callback_error).on_finished(
-        se_callback, updates_result=True
-    ).run().result() == 'Error'
+class TestCallbacks(object):
+    def test_on_finished_success_threading(self, rnt):
+        assert rnt.new_task(task_callback_success).on_finished(
+            se_callback, updates_result=True
+        ).run().result() == 'Success'
+
+    def test_on_finished_success_processing(self, rnp):
+        assert rnp.new_task(task_callback_success).on_finished(
+            se_callback, updates_result=True
+        ).run().result() == 'Success'
+
+    def test_on_finished_error_threading(self, rnt):
+        assert rnt.new_task(task_callback_error).on_finished(
+            se_callback, updates_result=True
+        ).run().result() == 'Error'
+
+    def test_on_finished_error_processing(self, rnp):
+        assert rnp.new_task(task_callback_error).on_finished(
+            se_callback, updates_result=True
+        ).run().result() == 'Error'
+
+    def test_on_success_threading(self, rnt):
+        assert rnt.new_task(task_callback_success).on_success(
+            success_callback, updates_result=True
+        ).run().result() == 'Success'
+
+    def test_on_success_processing(self, rnp):
+        assert rnp.new_task(task_callback_success).on_success(
+            success_callback, updates_result=True
+        ).run().result() == 'Success'
+
+    def test_on_error_threading(self, rnt):
+        assert rnt.new_task(task_callback_error).on_error(
+            error_callback, updates_result=True
+        ).run().result() == 'Error'
+
+    def test_on_error_processing(self, rnp):
+        assert rnp.new_task(task_callback_error).on_error(
+            error_callback, updates_result=True
+        ).run().result() == 'Error'
+
+    def test_on_iter_success_threading(self, rnt):
+        assert rnt.new_task(task_callback_success).on_iter(
+            se_callback, updates_result=True
+        ).run(times=3).result() == 'Success'
+
+    def test_on_iter_success_processing(self, rnp):
+        assert rnp.new_task(task_callback_success).on_iter(
+            se_callback, updates_result=True
+        ).run(times=3).result() == 'Success'
+
+    def test_on_iter_error_threading(self, rnt):
+        assert rnt.new_task(task_callback_error).on_iter(
+            se_callback, updates_result=True
+        ).run(times=3).result() == 'Error'
+
+    def test_on_iter_error_processing(self, rnp):
+        assert rnp.new_task(task_callback_error).on_iter(
+            se_callback, updates_result=True
+        ).run(times=3).result() == 'Error'
 
 
-def test_success_callback(rnt, rnp):
-    assert rnt.new_task(task_callback_success).on_success(
-        success_callback, updates_result=True
-    ).run().result() == 'Success'
-    assert rnp.new_task(task_callback_success).on_success(
-        success_callback, updates_result=True
-    ).run().result() == 'Success'
+class TestTasksList(object):
+    def test_count_threading(self, rnt):
+        r1 = rnt.new_task(simple_task)
+        tasks_prev_count = len(rnt.pending_tasks())
+        r1.run().result()
+        assert tasks_prev_count == 1
 
+    def test_count_finished_threading(self, rnt):
+        rnt.new_task(simple_task).run().result()
+        assert len(rnt.pending_tasks()) == 0
 
-def test_error_callback(rnt, rnp):
-    assert rnt.new_task(task_callback_error).on_error(
-        error_callback, updates_result=True
-    ).run().result() == 'Error'
-    assert rnp.new_task(task_callback_error).on_error(
-        error_callback, updates_result=True
-    ).run().result() == 'Error'
+    def test_count_processing(self, rnp):
+        r1 = rnp.new_task(simple_task)
+        tasks_prev_count = len(rnp.pending_tasks())
+        r1.run().result()
+        assert tasks_prev_count == 1
 
-
-def test_iter_success_callback(rnt, rnp):
-    assert rnt.new_task(task_callback_success).on_iter(
-        se_callback, updates_result=True
-    ).run(times=3).result() == 'Success'
-    assert rnp.new_task(task_callback_success).on_iter(
-        se_callback, updates_result=True
-    ).run(times=3).result() == 'Success'
-
-
-def test_iter_error_callback(rnt, rnp):
-    assert rnt.new_task(task_callback_error).on_iter(
-        se_callback, updates_result=True
-    ).run(times=3).result() == 'Error'
-    assert rnp.new_task(task_callback_error).on_iter(
-        se_callback, updates_result=True
-    ).run(times=3).result() == 'Error'
+    def test_count_finished_processing(self, rnp):
+        rnp.new_task(simple_task).run().result()
+        assert len(rnp.pending_tasks()) == 0
 
 
 # DUMMY TASKS =================================================================
